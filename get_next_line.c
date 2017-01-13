b5fe1f49f106725cce6d2dc005d0aca2da10d661
Victor BOIVIN <vboivin@e1r4p19.42.fr>
2017-01-13 16:39:06 +0100

    :(

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vboivin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/04 16:05:58 by vboivin           #+#    #+#             */
-/*   Updated: 2017/01/05 18:07:09 by vboivin          ###   ########.fr       */
+/*   Updated: 2017/01/13 16:38:35 by vboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,12 @@ int		getchr(int fd, char *a)
 	static int	bufpos = 0;
 	char		outp;
 
-	printf("2a\n");
 	if (bufchr == 0)
 	{
 		bufpos = 0;
 		buffed = malloc(BUFF_SIZE + 1);
-		printf("2b\n");
 		bufchr = read(fd, buffed, BUFF_SIZE);
 	}
-	printf("%d\n", bufchr);
 	if (bufchr == 0)
 		return (0);
 	if (bufchr == -1)
@@ -63,9 +60,7 @@ int		get_next_line(int fd, char **line)
 	if (outp == NULL || BUFF_SIZE <= 0 || fd < 0)
 		return (-1);
 	i = 0;
-	printf("1a\n");
 	errn = getchr(fd, &a);
-	printf("1b\n");
 	if (errn == -1 || errn == 0)
 		return (errn == -1) ? -1 : 0;
 	while (a != '\n' && a != '\0')
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vboivin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/05 17:52:04 by vboivin           #+#    #+#             */
-/*   Updated: 2017/01/05 18:07:12 by vboivin          ###   ########.fr       */
+/*   Updated: 2017/01/13 16:38:33 by vboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,20 @@ int		main(int ac, char **av)
 {
 	char	*str;
 	int		fd;
+	int fd2;
+	int	rez;
 
-	if (ac == 1)
+	if (ac <= 2)
 	{
 		printf("ac\n");
 		return 0;
 	}
+	fd = open(av[1], O_RDONLY);
+	fd2 = open(av[2], O_RDONLY);
 	printf("0a\n");
-	get_next_line(fd, &str);
+	rez = get_next_line(fd, &str);
+	printf("%s\n", str);
+	rez = get_next_line(fd2, &str);
 	printf("%s\n", str);
 	return 0;
 }
